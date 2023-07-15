61969d907848053f879108c234dcdfbc46a402fd
MyNameIsDylan99 <dylan.frosini@outlook.com>
2023-07-15 15:09:03 +0200

    Implemented Obj-Loader

--- SAE.OpenGL.S1/Program.cs
+++ SAE.OpenGL.S1/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7715c6c164da421ebdcb6a6da1f165679870f5122a0c8393c63c182e79acf963
-size 15635
+oid sha256:1fcdd15723836d5f643ac6c6f580ee081d62aebaf565abb78a4a7268357cde86
+size 16080
